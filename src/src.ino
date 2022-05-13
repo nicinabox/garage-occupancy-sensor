b7704ccfa548abfb02a03280744b1783baf60aff
Nic Haynes <nic@nicinabox.com>
2022-05-12 22:25:39 -0500

    Standard code formating

--- src/src.ino
+++ src/src.ino
@@ -14,107 +14,119 @@ RunningMedian samples = RunningMedian(5);
 
 float distanceCm, prevDistanceCm;
 
-void connectWifi() {
-  Serial.setDebugOutput(true);
-  Serial.println("Trying to connect " + String(WIFI_SSID));
+void connectWifi()
+{
+	Serial.setDebugOutput(true);
+	Serial.println("Trying to connect " + String(WIFI_SSID));
 
-  WiFi.mode(WIFI_STA);
-  WiFi.hostname(HOSTNAME);
+	WiFi.mode(WIFI_STA);
+	WiFi.hostname(HOSTNAME);
 
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+	WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(1000);
-  }
+	while (WiFi.status() != WL_CONNECTED)
+	{
+		Serial.print(".");
+		delay(1000);
+	}
 
-  Serial.println(WiFi.localIP());
+	Serial.println(WiFi.localIP());
 }
 
-void onWifiDisconnect(WiFiEvent_t event, WiFiEventInfo_t info) {
-  connectWifi();
+void onWifiDisconnect(WiFiEvent_t event, WiFiEventInfo_t info)
+{
+	connectWifi();
 }
 
-void connectMQTT() {
-  while (!client.connected()) {
-    Serial.print("Trying MQTT connection");
-    Serial.print(".");
+void connectMQTT()
+{
+	while (!client.connected())
+	{
+		Serial.print("Trying MQTT connection");
+		Serial.print(".");
 
-    bool connected = client.connect(
-      MQTT_CLIENT_ID,
-      MQTT_USER,
-      MQTT_PASS,
-      MQTT_LAST_WILL_TOPIC,
-      MQTT_LAST_WILL_QOS,
-      MQTT_LAST_WILL_RETAIN,
-      MQTT_LAST_WILL_MESSAGE
-    );
+		bool connected = client.connect(
+			MQTT_CLIENT_ID,
+			MQTT_USER,
+			MQTT_PASS,
+			MQTT_LAST_WILL_TOPIC,
+			MQTT_LAST_WILL_QOS,
+			MQTT_LAST_WILL_RETAIN,
+			MQTT_LAST_WILL_MESSAGE);
 
-    if (connected) {
-      Serial.println("connected");
-      client.publish(MQTT_TOPIC_CONNECTED, "true");
-      occupancyEffect(isPresent(distanceCm));
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      delay(1000);
-    }
-  }
+		if (connected)
+		{
+			Serial.println("connected");
+			client.publish(MQTT_TOPIC_CONNECTED, "true");
+			occupancyEffect(isPresent(distanceCm));
+		}
+		else
+		{
+			Serial.print("failed, rc=");
+			Serial.print(client.state());
+			delay(1000);
+		}
+	}
 }
 
-void occupancyEffect(bool isPresent) {
-    const char* value = isPresent ? OCCUPANCY_DETECTED_VALUE : OCCUPANCY_NOT_DETECTED_VALUE;
+void occupancyEffect(bool isPresent)
+{
+	const char *value = isPresent ? OCCUPANCY_DETECTED_VALUE : OCCUPANCY_NOT_DETECTED_VALUE;
 
-    digitalWrite(LED_BUILTIN, isPresent ? HIGH : LOW);
+	digitalWrite(LED_BUILTIN, isPresent ? HIGH : LOW);
 
-    client.publish(MQTT_TOPIC_OCCUPANCY, value);
+	client.publish(MQTT_TOPIC_OCCUPANCY, value);
 
-    Serial.print(MQTT_TOPIC_OCCUPANCY);
-    Serial.print(" - ");
-    Serial.println(value);
+	Serial.print(MQTT_TOPIC_OCCUPANCY);
+	Serial.print(" - ");
+	Serial.println(value);
 }
 
-bool isPresent(float distanceValue) {
-  return distanceValue > 0 && distanceValue <= MAX_PRESENCE_DISTANCE_CM;
+bool isPresent(float distanceValue)
+{
+	return distanceValue > 0 && distanceValue <= MAX_PRESENCE_DISTANCE_CM;
 }
 
-void setup() {
-  Serial.begin(115200);
+void setup()
+{
+	Serial.begin(115200);
 
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
+	pinMode(trigPin, OUTPUT);
+	pinMode(echoPin, INPUT);
 
-  pinMode(LED_BUILTIN, OUTPUT);
+	pinMode(LED_BUILTIN, OUTPUT);
 
-  connectWifi();
-  connectMQTT();
+	connectWifi();
+	connectMQTT();
 
-  WiFi.onEvent(onWifiDisconnect, ARDUINO_EVENT_WIFI_STA_DISCONNECTED);
+	WiFi.onEvent(onWifiDisconnect, ARDUINO_EVENT_WIFI_STA_DISCONNECTED);
 }
 
-void loop() {
-  connectMQTT();
-  client.loop();
+void loop()
+{
+	connectMQTT();
+	client.loop();
 
-  float rawDistance = distanceSensor.measureDistanceCm();
-  samples.add(rawDistance);
+	float rawDistance = distanceSensor.measureDistanceCm();
+	samples.add(rawDistance);
 
-  distanceCm = samples.getMedian();
+	distanceCm = samples.getMedian();
 
-  bool present = isPresent(distanceCm);
-  bool prevPresent = isPresent(prevDistanceCm);
+	bool present = isPresent(distanceCm);
+	bool prevPresent = isPresent(prevDistanceCm);
 
-  if (present != prevPresent) {
-      occupancyEffect(present);
+	if (present != prevPresent)
+	{
+		occupancyEffect(present);
 
-      Serial.print("distanceCm/prevDistanceCm - ");
-      Serial.print(distanceCm);
-      Serial.print("/");
-      Serial.print(prevDistanceCm);
-      Serial.println();
-  }
+		Serial.print("distanceCm/prevDistanceCm - ");
+		Serial.print(distanceCm);
+		Serial.print("/");
+		Serial.print(prevDistanceCm);
+		Serial.println();
+	}
 
-  prevDistanceCm = distanceCm;
+	prevDistanceCm = distanceCm;
 
-  delay(500);
+	delay(500);
 }
